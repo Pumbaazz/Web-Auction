67626b994e5d8c4a037afdd83b8c1539ab9e1a1a
Pumbaazz <nguyntunphng@gmail.com>
2019-12-07 00:34:10 +0700

    Revert "Merge branch 'master' of https://github.com/Pumbaazz/Web"
    This reverts commit f094613eaaa94b7da5f338a967692d7129e99350, reversing
    changes made to 2042b0eb0a9d5666d211cb26176754ae2163a034.

--- js/Auction_Update_ProductMoney.js
+++
@@ -1,8 +0,0 @@
-function Auction_ProductMoney()
-{
-    var money = document.getElementById('Product_Original_Money');
-    var money1 = money.textContent;
-    var money2 = parseFloat(money1);
-    UpMoney = money2 + 100000;
-    document.getElementById('Product_Auction_Money').textContent = UpMoney.toString();
-}
