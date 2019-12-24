5d75a60aaadaa78a5b158234144ed95781d66ee7
kien_nguyen <1753067.17clc@gmail.com>
2019-12-24 16:30:37 +0700

    add hbs

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,43 @@
 const express = require('express');
-
+const exphbs = require('express-handlebars');
 const app = express();
 
-// app.get('/', function(req, res){
-//     res.send('hello express');
-// })
+app.engine('hbs', exphbs({
+    defaultLayout:'main.hbs'
+
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+    res.render('home');
+})
+
+
+app.get('/about', function(req, res){
+    res.render('about');
+})
+
 
 var path = require('path');
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, `../html`, `homepage.html`));
+app.get('/homepage', function(req, res){
+    //res.sendFile(path.join(__dirname, `../html`, `homepage.html`));
+    res.render('homepage', {layout: false});
+})
+
+app.get('/admin/categories/', function(req, res){
+    const list = [
+        {CatID: 1, CatName: 'Laptop'},
+        {CatID: 2, CatName: 'Smartphone'},
+        {CatID: 3, CatName: 'Tablet'},
+        {CatID: 4, CatName: 'Clothes'},
+        {CatID: 5, CatName: 'Sneakers'},
+        {CatID: 6, CatName: 'Jewels'},
+
+    ]
+    res.render(`vwCategories/index`, {
+        categories: list
+    });
 })
 
 
