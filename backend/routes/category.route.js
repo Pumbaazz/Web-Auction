0394a6dc7a27772f759855982dcefbc792e376e0
kien_nguyen <1753067.17clc@gmail.com>
2019-12-26 11:29:01 +0700

    promisify

--- backend/routes/category.route.js
+++ backend/routes/category.route.js
@@ -1,35 +1,14 @@
 const express = require('express');
-const mysql = require('mysql');
+const db = require('../utils/db')
 
 const router = express.Router();
 
-router.get('/', function(req, res){
-    // const list = [
-    //     {CatID: 1, CatName: 'Laptop'},
-    //     {CatID: 2, CatName: 'Smartphone'},
-    //     {CatID: 3, CatName: 'Tablet'},
-    //     {CatID: 4, CatName: 'Clothes'},
-    //     {CatID: 5, CatName: 'Sneakers'},
-    //     {CatID: 6, CatName: 'Jewels'},
-
-    // ]
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        port: 8889,
-        user: 'root',
-        password: 'root',
-        database: 'qlbh'
-    });
-
-    connection.connect();
-    connection.query('select * from categories', function(error, result, fields){
-        if(error) throw error;
-        res.render(`vwCategories/index`, {
-        categories: result,
-        empty: result.length === 0
-        });
-        connection.end();
-    });
+router.get('/', async function(req, res){
+    const results = await db.load('select * from categories');
+    res.render(`vwProducts/index`, {
+        products: results,
+        empty: results.length === 0
+    })
 })
 
 router.get('/add', function(req, res){
@@ -38,4 +17,3 @@ router.get('/add', function(req, res){
 })
 
 module.exports = router;
-
